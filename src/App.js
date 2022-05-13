b3f59d26cf340b8ae5b98671ed344d0a520232bb
erecoliza <eduardo.recoliza@gmail.com>
2022-05-13 00:29:38 -0300

    varias modificaciones

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //import logo from './logo.svg';
 import logo from './LOGO-NUEVO-180x180.png';
+//import "./reset.css";
 import './App.css';
 import autos from './Autos.json';
 
@@ -7,25 +8,27 @@ import autos from './Autos.json';
   { id: 1, nombre: "Corsa", modelo: "Chevrolet"},
   { id: 2, nombre: "Siena", modelo: "Fiat"}, 
 ]
+// autos && autos.map(auto =>  verifca si hay datos antes de hacer el map "
 */
 
 function App() {
   return (
     <div className="App" >
-      <header className="App-header" style={{ backgroundColor: "rgb(0, 156, 161)"}} >
+      <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-
         <h2>Bienvenidos a la Clase 1 de React</h2>  
-        <div className="box">
-            { autos.map(auto => (
+        
+        <div className="box">            
+            { autos && autos.map(auto => (
               <div key={auto.id}>({auto.id}) {auto.modelo} <br />              
                {auto.nombre} </div>
             ))}     
         </div>
+        <a href="http://www.autos.com.ar" className="App-link" target="noreferrer">Autos</a> 
         <ul>
-          { autos.map(auto => (<li>{auto.modelo} - {auto.nombre} </li>)     )}
-        </ul> 
-      </header>
+          { autos.map(auto => (<li>{auto.modelo} - {auto.nombre} </li>)    )}          
+        </ul>        
+      </header>      
     </div>
   );
 }
